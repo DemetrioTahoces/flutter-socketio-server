7de52f57251f47354aa64b0762d8e95c425b93a4
tonitahoces <tonitahoces@gmail.com>
2021-01-14 17:57:26 +0100

    Fin Seccion 3

---
+++ public/index.js
@@ -0,0 +1,15 @@
+var socket = io();
+
+socket.on('connect', function() {
+    console.log('Conectado al sevidor');
+});
+
+socket.on('disconnect', function() {
+    console.log('Perdimos conexion con el sevidor');
+});
+
+socket.on('mensaje', function(message) {
+    console.log(message);
+});
+
+socket.emit('mensaje', { nombre: 'Demetrio' });
---
+++ sockets/socket.js
@@ -0,0 +1,15 @@
+const { io } = require('../index');
+
+// Mensajes de sockets
+io.on('connection', client => {
+    console.log('cliente conectado');
+
+    client.on('disconnect', () => {
+        console.log('cliente desconectado');
+    });
+
+    client.on('mensaje', (message) => {
+        console.log(message);
+        io.emit('mensaje', { admin: 'nuevo mensaje' });
+    });
+});
